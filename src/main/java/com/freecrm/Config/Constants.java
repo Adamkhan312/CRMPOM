9ce718614a1820b569445bc437aa25c4dd087762
adamkhan312 <akkhan312@gmail.com>
2020-07-07 18:01:23 -0400

    TEST PW UPDATED

--- src/main/java/com/freecrm/Config/Constants.java
+++ src/main/java/com/freecrm/Config/Constants.java
@@ -3,7 +3,7 @@ package com.freecrm.Config;
 public class Constants {
 
     public static final String email = "akkhan312@gmail.com";
-    public static final String password = "Balling1!";
+    public static final String password = "Test12345!";
 
 
 }
