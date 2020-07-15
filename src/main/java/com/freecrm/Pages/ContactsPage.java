4753353c314c410b22a80168ba83f58b34dcfb16
adamkhan312 <akkhan312@gmail.com>
2020-07-14 21:19:54 -0400

    Started On sorting of ColumnHEader functionality

--- src/main/java/com/freecrm/Pages/ContactsPage.java
+++ src/main/java/com/freecrm/Pages/ContactsPage.java
@@ -3,12 +3,14 @@ package com.freecrm.Pages;
 import com.freecrm.Base.BasePage;
 import com.freecrm.Config.Constants;
 import com.freecrm.Utilities.Xls_Reader;
+import com.google.common.collect.Ordering;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.List;
 public class ContactsPage extends BasePage {
 
     Xls_Reader excel;
+    int countsMatched=0;
 
     //---------------------------------------------------Constructor----------------------------------------------------//
     public ContactsPage(WebDriver driver) {
@@ -31,6 +34,14 @@ public class ContactsPage extends BasePage {
     @FindBy(css = "thead.full-width tr > *")
     List<WebElement> contactHeader;
 
+    @FindBy(xpath = "//th[contains(text(),'Name')]")
+    WebElement NameHeader;
+
+    @FindBy(xpath = "//tr[1]//td[2]")
+    WebElement firstContactName;
+
+    By nameCaptured = By.xpath("//tr[1]//td[2]");
+
 
     //---------------------------------------------------Getters for By-------------------------------------------------//
 
@@ -43,11 +54,36 @@ public class ContactsPage extends BasePage {
         loginPage.clickOnContactsMenu();
     }
 
+
+    public void clickOnNameHeader(){
+        NameHeader.click();
+    }
+
+    public boolean compareSort(){
+        ArrayList<String> names = new ArrayList<>();
+        for(int i=0;i<2;i++){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clickOnNameHeader();
+            wait.until(ExpectedConditions.refreshed(ExpectedConditions.visibilityOf(firstContactName)));
+            names.add(firstContactName.getText());
+        }
+        System.out.println(names.toString());
+        boolean isSorted = Ordering.from(String.CASE_INSENSITIVE_ORDER).isOrdered(names);
+        System.out.println(isSorted);
+            return isSorted;
+
+
+    }
     public ArrayList<String> getColumnHeaders() {
         ArrayList<String> list = new ArrayList<String>();
+        List<WebElement> headerlist = contactHeader;
         try {
             for (int i = 0; i < contactHeader.size(); i++) {
-              list.add(contactHeader.get(i).getText());
+              list.add(contactHeader.get(i).getAttribute("innerText"));
             }
         } catch (Exception e) {
             System.out.println("couldn't find element");
@@ -58,11 +94,48 @@ public class ContactsPage extends BasePage {
 
 
     public void verifyColumnHeaders() {
+        wait.until(ExpectedConditions.visibilityOfAllElements(contactHeader));
+                ArrayList<String> col = getColumnHeaders();
+        System.out.println("Size of list is "+ col.size());
+        for (int i = 0; i < col.size(); i++) {
+            System.out.println("Header value is " + col.get(i));
+        }
+
+    }
+    public boolean verifyColumnHeaders1( String value) {
+        int countsMatched=0;
+        wait.until(ExpectedConditions.visibilityOfAllElements(contactHeader));
         ArrayList<String> col = getColumnHeaders();
         System.out.println("Size of list is "+ col.size());
         for (int i = 0; i < col.size(); i++) {
-            System.out.println("Header value is " + getColumnHeaders().get(i));
+            System.out.println("Header value is " + col.get(i));
+            if(value.equals(col.get(i))){
+                countsMatched++;
+                System.out.println("Value found is "+ col.get(i)+ " value expected is "+ value);
+            }else{
+                countsMatched--;
+                System.out.println("Value found is "+ col.get(i)+ "Value expected is "+ value);
+            }
+
         }
+        if(countsMatched==8){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public boolean verifyColumnHeaders(ArrayList<String> list, String value) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(contactHeader));
+//        ArrayList<String> col = getColumnHeaders();
+        list.toString();
+       if(list.contains(value)){
+           System.out.println("Value expected is "+ value);
+           return true;
+       }else{
+           return false;
+       }
 
     }
 }
--- src/test/java/com/freecrm/TestCases/ContactPageTests.java
+++ src/test/java/com/freecrm/TestCases/ContactPageTests.java
@@ -3,7 +3,11 @@ package com.freecrm.TestCases;
 import com.freecrm.Config.Constants;
 import com.freecrm.Pages.ContactsPage;
 import com.freecrm.TestBase.BaseTest;
+import com.freecrm.Utilities.DataProviders;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Hashtable;
 
 public class ContactPageTests extends BaseTest {
 
@@ -13,12 +17,18 @@ public class ContactPageTests extends BaseTest {
     }
 
 
-    @Test
-    public void verifyColumnHeadersTest(){
+    @Test(dataProvider = "Data", dataProviderClass = DataProviders.class,enabled = false)
+    public void verifyContactHeadersTEST(Hashtable<String,String> data){
         ContactsPage contactsPage = new ContactsPage(driver);
         contactsPage.navigateToContactsPage();
-        contactsPage.getColumnHeaders();
-        contactsPage.verifyColumnHeaders();
+        //Assert.assertTrue(contactsPage.verifyColumnHeaders(data.get("ContactHeader")));
+    }
+
+    @Test(dataProvider = "Data", dataProviderClass = DataProviders.class,enabled=false)
+    public void verifyContactHeaders(Hashtable<String,String> data){
+        ContactsPage contactsPage = new ContactsPage(driver);
+        contactsPage.navigateToContactsPage();
+        contactsPage.verifyColumnHeaders(contactsPage.getColumnHeaders(),data.get("ContactHeader"));
     }
 
 
@@ -32,8 +42,14 @@ public class ContactPageTests extends BaseTest {
 
     }
 
-    @Test(enabled = false)
+    @Test
     public void verifyContactNameIsSortableTest(){
+        ContactsPage contactsPage = new ContactsPage(driver);
+        contactsPage.navigateToContactsPage();
+        contactsPage.clickOnNameHeader();
+        contactsPage.clickOnNameHeader();
+        contactsPage.compareSort();
+
 
     }
 
