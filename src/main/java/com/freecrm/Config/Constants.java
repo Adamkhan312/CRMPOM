4b84187c0d6b8b4b362b9187eb982a6e5bc0d935
adamkhan312 <akkhan312@gmail.com>
2020-07-22 20:50:06 -0400

    config

---
+++ src/main/java/com/freecrm/Config/Constants.java
@@ -0,0 +1,10 @@
+package com.freecrm.Config;
+
+public class Constants {
+
+
+   public  final static String password="Test12345!";
+    public static String email="akkhan312@gmail.com";;
+    public final static int todaysFontWeight = 700;
+    public final static int fontWeight = 300;
+}
